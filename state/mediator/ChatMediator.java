9316b31a4b0295ba4eaa67e813872eed8f67846a
Kaio <kaio.bezerra1809@gmail.com>
2026-01-15 16:29:36 -0300

    Delete state/mediator directory

--- state/mediator/ChatMediator.java
+++
@@ -1,7 +0,0 @@
-package org.example.mediator;
-
-public interface ChatMediator {
-
-    void enviarMensagem(String mensagem, Usuario remetente, Usuario destinatario);
-    void adicionarUsuario(Usuario usuario);
-}
