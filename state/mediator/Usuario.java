5f75569381511e0c102df4e5d63e4571149a025d
Kaio <kaio.bezerra1809@gmail.com>
2026-01-15 16:30:19 -0300

    Add files via upload

---
+++ state/mediator/Usuario.java
@@ -0,0 +1,24 @@
+package org.example.mediator;
+
+public class Usuario {
+
+    private String nome;
+    private ChatMediator mediator;
+
+    public Usuario(String nome, ChatMediator mediator) {
+        this.nome = nome;
+        this.mediator = mediator;
+    }
+
+    public void enviaMensagem(String mensagem, Usuario destinatario) {
+        mediator.enviarMensagem(mensagem, this, destinatario);
+    }
+
+    public void receberMensagem(String mensagem, Usuario remetente) {
+        System.out.println(nome + " recebeu de " + remetente.getNome() + ": " + mensagem);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
